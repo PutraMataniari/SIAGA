5317608d327d10137a945044b6fced5818bb2ada
Putra Mataniari <trisaputrasimatupang@gmail.com>
2025-10-01 22:37:11 +0700

    buat dan perbaikan kode otp

--- app/src/main/java/com/example/siaga/view/history/HistoryAdapter.kt
+++ app/src/main/java/com/example/siaga/view/history/HistoryAdapter.kt
@@ -15,7 +15,7 @@
     import com.example.siaga.view.model.HistoryResponse
 
     object Constants {
-        const val BASE_URL_IMAGE = "http://192.168.1.11:8000/storage/"
+        const val BASE_URL_IMAGE = "http://192.168.1.12:8000/storage/"
     }
 
 
